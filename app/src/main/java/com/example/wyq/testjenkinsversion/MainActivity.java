c2ed93b13fca95074486d73af717b19f53555e9e
WANGYONGQIANG\wyq <wangyq_mail@163.com>
2018-05-30 16:20:59 +0800

    第二次更新

--- app/src/main/java/com/example/wyq/testjenkinsversion/MainActivity.java
+++ app/src/main/java/com/example/wyq/testjenkinsversion/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.wyq.testjenkinsversion;
 
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView version = this.findViewById(R.id.version);
+        try {
+            String versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+            int versionCode = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
+            version.setText("当前的版本名是："+versionName+"  当前的版本号是:"+versionCode);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
